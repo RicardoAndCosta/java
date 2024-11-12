44aa8efeb4e22cc3684de83b6460b173de5c0737
Ricardo Andrade <157977009+RicardoAndCosta@users.noreply.github.com>
2024-11-12 18:25:40 -0300

    Criação de consultas utilizando JPA Query Methods

--- minhas_tarefas/src/main/java/br/com/tarefas/minhas_tarefas/controller/TarefaController.java
+++ minhas_tarefas/src/main/java/br/com/tarefas/minhas_tarefas/controller/TarefaController.java
@@ -1,6 +1,7 @@
 package br.com.tarefas.minhas_tarefas.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class TarefaController {
@@ -21,8 +23,13 @@ public class TarefaController {
     private TarefaRepository repositorio;
 
     @GetMapping("/tarefa")
-    public List<Tarefa> todasTarefas() {
-        return repositorio.findAll();
+    public List<Tarefa> todasTarefas(@RequestParam Map<String, String> parametros) {
+        if (parametros.isEmpty())
+            return repositorio.findAll();
+
+        String descricao = parametros.get("descricao");
+        return repositorio.findByDescricaoLike("%" + descricao + "%");
+        
     }
 
     @GetMapping("/tarefa/{id}")
--- minhas_tarefas/src/main/java/br/com/tarefas/minhas_tarefas/repository/TarefaRepository.java
+++ minhas_tarefas/src/main/java/br/com/tarefas/minhas_tarefas/repository/TarefaRepository.java
@@ -1,9 +1,17 @@
 package br.com.tarefas.minhas_tarefas.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.tarefas.minhas_tarefas.model.Tarefa;
 
 public interface TarefaRepository extends JpaRepository<Tarefa, Integer> {
+
+    // Desta forma só será retornado caso seja informado nos parametros o nome exato da tarefa
+    public List<Tarefa> findByDescricao(String descricao);
+
+    // Desta forma serão retornados as tarefas que tenham parte do nome da tarefa que foram informados nos parametros
+    public List<Tarefa> findByDescricaoLike(String descricao);
 
 }
